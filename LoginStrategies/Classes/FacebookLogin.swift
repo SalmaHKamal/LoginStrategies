e92fa8332137bfbe1b1ed0f156d40602d5bd25cf
Salma Hassan <salmahassan087@gmail.com>
2022-03-08 22:22:40 +0200

    First pod commit

---
+++ LoginStrategies/Classes/FacebookLogin.swift
@@ -0,0 +1,22 @@
+//
+//  FacebookLogin.swift
+//  LoginStrategies
+//
+//  Created by Salma Hassan on 08/03/2022.
+//
+
+import Foundation
+import UIKit
+
+public class FacebookLogin {
+	
+	public init() {}
+	
+	public func testPublic() {
+		print("I'm public")
+	}
+	
+	func testPrivate() {
+		print("private")
+	}
+}
